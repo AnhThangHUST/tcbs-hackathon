feb34c4c9cb497d84bcab09c02e2a831566bee66
cuongvv2 <cuongvv2@tcbs.com.vn>
2022-11-05 16:53:27 +0700

    cuongvv2

--- service/history.py
+++ service/history.py
@@ -1,15 +1,21 @@
 from service import document_crud
 from service.model import SearchResponse
 from storage import repository_executor
-from storage.entity import HistoryEntity
-
 
 def getHistotyOfTcbsId(tcbsid):
     query_res = repository_executor.getHistoryByTcbsId(tcbsid)
     idsDataRow = []
     for r in query_res:
         idsDataRow.append(r.data_raw_id)
-    return [SearchResponse(d) for d in document_crud.getDocumentWhereIdIn(idsDataRow)]
+
+    document = document_crud.getDocumentWhereIdIn(idsDataRow)
+
+    filteredIWC = filter(lambda data: data.source_type == 'iwealth_club', document)
+    filteredTCIV = filter(lambda data: data.source_type == 'tcinvest', document)
+
+    result = [filteredIWC[0:5], filteredTCIV[0:5]]
+
+    return [SearchResponse(d) for d in result]
 
 
 def insertHistory(drawDataId, tcbsid):
